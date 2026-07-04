5f51de2b98a4cd72445166921cea94cd9b52755c
Leo1267-hub <leonidberdin1@gmail.com>
2026-07-04 16:37:31 +0100

    Refactor authentication routes to include user login functionality; enhance error handling and response messages

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends,HTTPException, Response
 from authx import AuthX, AuthXConfig
-from backend.schemas.auth import UserCreate
+from backend.schemas.auth import UserCreate,UserLogin
 from backend.config import settings
 from backend.database import get_db
 from backend.models import User
@@ -22,8 +22,8 @@ auth = AuthX(config=config)
 
 @router.post('/signup')
 async def signup(credentials: UserCreate, db: AsyncSession = Depends(get_db),response:Response=None):
-    stmt = select(User).where((User.username == credentials.username) | (User.email == credentials.email))
-    result = await db.execute(stmt)
+    query = select(User).where((User.username == credentials.username) | (User.email == credentials.email))
+    result = await db.execute(query)
     user = result.scalar_one_or_none()
     if user:
         raise HTTPException(400, detail="Username or email already registered")
@@ -44,16 +44,19 @@ async def signup(credentials: UserCreate, db: AsyncSession = Depends(get_db),res
     return {"message": "User created successfully", "user_id": new_user.id}
 
 @router.post('/login')
-def login(credentials: UserCreate,response:Response):
-    if credentials.username == "testuser" and credentials.password == "testpassword":
-        access_token = auth.create_access_token(uid=credentials.username)
-        response.set_cookie(
-            key=config.JWT_ACCESS_COOKIE_NAME,
-            value=access_token)
-        response = {"access_token": access_token}
-        return response
-    
-    raise HTTPException(401, detail="Invalid credentials")
+async def login(credentials: UserLogin,response:Response,db: AsyncSession = Depends(get_db)):
+    query = select(User).where(User.username == credentials.username)
+    result = await db.execute(query)
+    user = result.scalar_one_or_none()
+    if user == None:
+        raise HTTPException(400, detail="Username doesnt exist")
+    if not verify_password(credentials.password,user.hashed_password):
+        raise HTTPException(400, detail="Wrong password")
+    access_token = auth.create_access_token(uid=credentials.username)
+    response.set_cookie(key=config.JWT_ACCESS_COOKIE_NAME,
+                        value=access_token
+                        )
+    return {"message": "successfully", "user_id": user.id}
 
 @router.get("/protected", dependencies=[Depends(auth.access_token_required)])
 def protected():
